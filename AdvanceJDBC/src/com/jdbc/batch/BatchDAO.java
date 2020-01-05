6aa759b13a293d399db21e47dcc389b0968dd9e6
Ritesh Kumar <riteshk6341@gmail.com>
2020-01-05 16:50:50 +0530

    batch_update

---
+++ AdvanceJDBC/src/com/jdbc/batch/BatchDAO.java
@@ -0,0 +1,31 @@
+package com.jdbc.batch;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class BatchDAO {
+	public static void main(String[] args) {
+
+		try (Connection conn = DriverManager
+				.getConnection("jdbc:mysql://localhost:3306/udemy?user=root&password=password");
+				Statement st = conn.createStatement();) {
+
+			st.addBatch("insert into account values(1,'Yadav','Khusi',20000)");
+			st.addBatch("insert into account values(2,'Yadav','Ritesh',10000)");
+
+			int[] result = st.executeBatch();
+
+			for (int i = 0; i < result.length; i++) {
+				System.out.println(result[i]);
+
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
